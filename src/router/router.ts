c22882b20dbb8843f08eb1093d459e92a21680dc
lixingyang <xylees@qq.com>
2022-09-30 22:01:15 +0800

    修改组件导入方式

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,24 +1,30 @@
 import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from '../views/HomeView.vue';
-import AboutView from '../views/AboutView.vue';
-import LoginView from '../views/LoginView.vue';
+// import HomeView from '../views/HomeView.vue';
+// import AboutView from '../views/AboutView.vue';
+// import LoginView from '../views/LoginView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/home',
     name: 'home',
-    component: defineAsyncComponent(() => import(/* @vite-ignore */ `${HomeView}`))
+    // component: defineAsyncComponent(() => import(/* @vite-ignore */ `${HomeView}`))
+    component: defineAsyncComponent(() => import(/* @vite-ignore */ '../views/HomeView.vue'))
+    // component: HomeView
   },
   {
     path: '/about',
     name: 'about',
-    component: defineAsyncComponent(() => import(/* @vite-ignore */ `${AboutView}`))
+    // component: defineAsyncComponent(() => import(/* @vite-ignore */ `${AboutView}`))
+    component: defineAsyncComponent(() => import(/* @vite-ignore */ '../views/AboutView.vue'))
+    // component: AboutView
   },
   {
     path: '/login',
     name: 'login',
-    component: defineAsyncComponent(() => import(/* @vite-ignore */ `${LoginView}`))
+    // component: defineAsyncComponent(() => import(/* @vite-ignore */ `${LoginView}`))
+    component: defineAsyncComponent(() => import(/* @vite-ignore */ '../views/LoginView.vue'))
+    // component: LoginView
   }
 ]
 
